b066445de282e07c4e41c265c531f9ab64450595
BillFoland <william.foland@colorado.edu>
2017-10-06 19:26:08 -0600

    Update daisylu_config.py

--- system/daisylu_config.py
+++ system/daisylu_config.py
@@ -7,15 +7,21 @@ def getSystemPath(tag):
     paths['NNModels']       = '../networks'
     paths['daisylu']        = '../'
     paths['daisyluPython']  = os.getcwd()
-    
-    # mac settings
-    if (platform.system() == 'Darwin'):
+ 
+    # Modify these settings for your system (mac or linux or both)
+    if (platform.system() == 'Darwin'): #mac settings
+        # path to python 2.7 executable
         paths['python']       = '/Users/bill_foland/anaconda/bin/python '  
-        paths['Wikifier2013'] = '/Users/bill_foland/Desktop/CU/PhD/Wikifier2013'
+        # path to Illinois wikifier
+        paths['Wikifier2013'] = '/Users/bill_foland/Wikifier2013'
+        # command line to run smatch
         paths['smatchCommand'] = 'python ../../smatch_2.0/smatch.py'
     else: # linux settings
+        # path to python 2.7 executable
         paths['python']       = 'python '   
+        # path to Illinois wikifier
         paths['Wikifier2013'] = '/home/bill/Wikifier2013'
+        # command line to run smatch
         paths['smatchCommand'] = 'python /home/bill/smatch_2.0/smatch.py'
     return paths[tag]
 
